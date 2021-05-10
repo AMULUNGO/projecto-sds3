86adbe440b566e5f803c573dbf436df953678209
AMulungo <mulungo.ali07@gmail.com>
2021-05-10 21:48:49 +0200

    Pageable

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pagable){
+		sellerRepository.findAll();//para que o jpa nao busque constantimente todas venda, basta colocar os
+		//seller na memoria.A jpa vai armazenar os sellers em cash e depois
+		Page<Sale> result = repository.findAll(pagable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
